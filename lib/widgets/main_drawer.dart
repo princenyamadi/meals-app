8b5d02a05086e3e02bc41b1543bf086bcce3ae0d
princenyamadi <princenyamadi.pn@gmail.com>
2021-02-06 23:26:32 +0000

    feat: added main drawer widget

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Theme.of(context).primaryColor),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.restaurant,
+              size: 36,
+            ),
+            title: Text(
+              'Meal',
+              style: TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              size: 36,
+            ),
+            title: Text(
+              'Filter',
+              style: TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
