7ac0919deb37db573c6f3dcfb206aa09d455f219
princenyamadi <princenyamadi.pn@gmail.com>
2021-02-13 22:12:29 +0000

    feat: added floating action button for deleting items and passing data back

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -36,60 +36,67 @@ class MealDetailScreen extends StatelessWidget {
     }
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('${selectedMeal.title}'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 300,
-                width: double.infinity,
-                child: Image.network(
-                  selectedMeal.imageUrl,
-                  fit: BoxFit.cover,
-                ),
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
               ),
-              buildSectionTitle(
-                context: context,
-                text: 'Ingredients',
-              ),
-              buildContainer(
-                child: ListView.builder(
-                  itemCount: selectedMeal.ingredients.length,
-                  itemBuilder: (ctx, index) => Card(
-                    color: Theme.of(context).accentColor,
-                    child: Padding(
-                      padding: EdgeInsets.all(20.0),
-                      child: Text(
-                        selectedMeal.ingredients[index],
-                      ),
+            ),
+            buildSectionTitle(
+              context: context,
+              text: 'Ingredients',
+            ),
+            buildContainer(
+              child: ListView.builder(
+                itemCount: selectedMeal.ingredients.length,
+                itemBuilder: (ctx, index) => Card(
+                  color: Theme.of(context).accentColor,
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      selectedMeal.ingredients[index],
                     ),
                   ),
                 ),
               ),
-              buildSectionTitle(context: context, text: 'Steps'),
-              buildContainer(
-                  child: ListView.builder(
-                      itemCount: selectedMeal.steps.length,
-                      itemBuilder: (ctx, index) => Column(
-                            children: [
-                              ListTile(
-                                leading: CircleAvatar(
-                                  child: Text('# ${index + 1}'),
-                                ),
-                                title: Text(
-                                  selectedMeal.steps[index],
-                                ),
+            ),
+            buildSectionTitle(context: context, text: 'Steps'),
+            buildContainer(
+                child: ListView.builder(
+                    itemCount: selectedMeal.steps.length,
+                    itemBuilder: (ctx, index) => Column(
+                          children: [
+                            ListTile(
+                              leading: CircleAvatar(
+                                child: Text('# ${index + 1}'),
                               ),
-                              Divider(
-                                height: 3,
-                                color: Theme.of(context).primaryColor,
-                              )
-                            ],
-                          ))),
-            ],
-          ),
-        ));
+                              title: Text(
+                                selectedMeal.steps[index],
+                              ),
+                            ),
+                            Divider(
+                              height: 3,
+                              color: Theme.of(context).primaryColor,
+                            )
+                          ],
+                        ))),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.delete),
+        onPressed: () {
+          Navigator.of(context).pop(mealId);
+        },
+      ),
+    );
   }
 }
