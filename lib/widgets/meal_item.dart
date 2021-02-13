2ff7559bc58d14db59b79b103e8f82146441e173
princenyamadi <princenyamadi.pn@gmail.com>
2021-02-13 22:15:10 +0000

    feat: added new function to mealItem

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -9,7 +9,7 @@ class MealItem extends StatelessWidget {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
-
+  final Function removeItem;
   MealItem({
     @required this.id,
     @required this.affordability,
@@ -17,6 +17,7 @@ class MealItem extends StatelessWidget {
     @required this.duration,
     @required this.imageUrl,
     @required this.title,
+    @required this.removeItem,
   });
 
   String get complexityText {
@@ -55,7 +56,15 @@ class MealItem extends StatelessWidget {
   }
 
   void selectMeal(BuildContext context) {
-    Navigator.of(context).pushNamed(MealDetailScreen.routeName, arguments: id);
+    Navigator.of(context)
+        .pushNamed(MealDetailScreen.routeName, arguments: id)
+        .then((result) {
+      print(result);
+
+      if (result != null) {
+        // removeItem(result);
+      }
+    });
   }
 
   @override
