e40eb11d829563fac1b3cd7cc6f898f20e9947af
princenyamadi <princenyamadi.pn@gmail.com>
2021-02-13 22:02:51 +0000

    feat: switched from stl to stf, added didChangeDependencies, added a function to remove an item from a list

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,39 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
-class CategoryMealsScreen extends StatelessWidget {
+class CategoryMealsScreen extends StatefulWidget {
   static const routeName = '/category-meals';
 
-  // final categoryId;
-  // final categoryTitle;
-
-  // CategoryMealsScreen({this.categoryId, this.categoryTitle});
   @override
-  Widget build(BuildContext context) {
-    final routeArgs =
+  _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  String categoryTitle;
+  List<Meal> displayedMeals;
+
+  var _loaddedInitData = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _removeMeal({String mealId}) {
+    setState(() {
+      displayedMeals.removeWhere((meal) => meal.id == mealId);
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    if(!_loaddedInitData){
+      final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
 
     final categoryId = routeArgs['id'];
-    final categoryTitle = routeArgs['title'];
-    final categoryMeals = DUMMY_MEALS.where((meal) {
+    categoryTitle = routeArgs['title'];
+    displayedMeals = DUMMY_MEALS.where((meal) {
       return meal.categories.contains(categoryId);
+      
     }).toList();
+    }
+    
+    _loaddedInitData = true;
+    super.didChangeDependencies();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(categoryTitle),
         ),
         body: ListView.builder(
-          itemCount: categoryMeals.length,
+          itemCount: displayedMeals.length,
           itemBuilder: (ctx, index) {
             return MealItem(
-              id: categoryMeals[index].id,
-              title: categoryMeals[index].title,
-              duration: categoryMeals[index].duration,
-              affordability: categoryMeals[index].affordability,
-              complexity: categoryMeals[index].complexity,
-              imageUrl: categoryMeals[index].imageUrl,
+              removeItem: _removeMeal,
+              id: displayedMeals[index].id,
+              title: displayedMeals[index].title,
+              duration: displayedMeals[index].duration,
+              affordability: displayedMeals[index].affordability,
+              complexity: displayedMeals[index].complexity,
+              imageUrl: displayedMeals[index].imageUrl,
             );
           },
         ));
