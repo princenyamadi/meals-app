ad10f540a9c34771ba42bbed78bcb81701e2da1d
princenyamadi <princenyamadi.pn@gmail.com>
2021-02-13 22:09:38 +0000

    feat: added route to Filter Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories_meals_screen.dart';
 import 'package:meals_app/screens/categories_screen.dart';
+import 'package:meals_app/screens/filters_screen.dart';
 import 'package:meals_app/screens/meal_detail_screen.dart';
 import 'package:meals_app/screens/tabs_screen.dart';
 
@@ -35,9 +36,10 @@ class MyApp extends StatelessWidget {
       home: TabsScreen(),
       // initialRoute: '/',
       routes: {
-        // '/': (ctx) => CategoriesScreen(),
+        // '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       },
     );
   }
